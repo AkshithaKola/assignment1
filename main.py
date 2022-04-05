4bec35ce4f160d2dff37d111bcebe441bd201c8c
AkshithaKola <102524785+AkshithaKola@users.noreply.github.com>
2022-04-05 16:07:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
+# z is the number of favourable outcomes
+# a,b,c are variables to count odd numbers, numbers divisible by two and three, numbers less than 16
+
+#probability of odd numbers
 z=0
 for a in range(0,26):
  if a%2==1:
   z=z+1
 
-
+# probability of numbers divisible by both 2 and 3 
 print(z/25)
 z=0
 for b in range(1,26):
  if b%6==0:
   z=z+1
 
- 
+# probability of numbers less than 16 
 print(z/25)
 z=0
 for c in range(1,26):
