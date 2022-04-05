2bad04222ef77fa304be981dccf16dc671e19cb6
AkshithaKola <102524785+AkshithaKola@users.noreply.github.com>
2022-04-05 16:01:45 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+z=0
+for a in range(0,26):
+ if a%2==1:
+  z=z+1
+
+
+print(z/25)
+z=0
+for b in range(1,26):
+ if b%6==0:
+  z=z+1
+
+ 
+print(z/25)
+z=0
+for c in range(1,26):
+ if c<16:
+  z=z+1
+
+
+print(z/25)
